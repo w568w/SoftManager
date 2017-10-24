ab712e6f13b569fb3a6f1c79d5003f6c681a6ef3
HumorSmith <wuyihua86@gmail.com>
2017-10-24 22:35:40 +0800

    删除多余的SoftFragment

--- app/src/main/java/cn/ifreedomer/com/softmanager/activity/HomeActivity.java
+++ app/src/main/java/cn/ifreedomer/com/softmanager/activity/HomeActivity.java
@@ -1,27 +1,20 @@
 package cn.ifreedomer.com.softmanager.activity;
 
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ExpandableListView;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import cn.ifreedomer.com.softmanager.R;
-import cn.ifreedomer.com.softmanager.SoftFragment;
+import cn.ifreedomer.com.softmanager.fragment.soft.SoftFragment;
 
 /**
  * @author HomorSmith
