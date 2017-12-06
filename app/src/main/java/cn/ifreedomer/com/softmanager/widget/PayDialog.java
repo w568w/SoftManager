7bcfff501eca7a215413762af19a84cba767fb0d
HumorSmith <wuyihua86@gmail.com>
2017-12-06 23:26:23 +0800

    充值之后.关闭充值接口

--- app/src/main/java/cn/ifreedomer/com/softmanager/widget/PayDialog.java
+++ app/src/main/java/cn/ifreedomer/com/softmanager/widget/PayDialog.java
@@ -135,6 +135,7 @@ public class PayDialog extends Dialog implements View.OnClickListener {
                     // 判断resultStatus 为9000则代表支付成功
                     if (TextUtils.equals(resultStatus, "9000")) {
                         // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
+                        GlobalDataManager.getInstance().setOpenRecharge(false);
                         Toast.makeText(PayDialog.this.getOwnerActivity(), "支付成功", Toast.LENGTH_SHORT).show();
                     } else {
                         // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
